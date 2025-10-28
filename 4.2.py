4c4bd61debbc5f6e77c68d517ea01962b566d5f8
artemkoleda1 <Koleda_AV1_25@student.grsu.by>
2025-10-28 09:19:26 +0300

    рамка и вывод

--- 4.2.py
+++ 4.2.py
@@ -8,3 +8,24 @@ def draw_right_triangle(rows, ch):
         for j in range(i):
             print(ch, end="")
         print()
+def draw_frame(rows, columns, ch):
+    for i in range(rows):
+        for j in range(columns):
+            if i == 0 or i == rows - 1 or j == 0 or j == columns - 1:
+                print(ch, end="")
+            else:
+                print(" ", end="")
+        print()
+
+# Основная программа
+n = int(input("Введите количество строк: "))
+m = int(input("Введите количество столбцов: "))
+
+print("\nПРЯМОУГОЛЬНИК:")
+draw_rectangle(n, m, "#")
+
+print("\nПРАВЫЙ ТРЕУГОЛЬНИК:")
+draw_right_triangle(n, "#")
+
+print("\nРАМКА:")
+draw_frame(n, m, "#")
