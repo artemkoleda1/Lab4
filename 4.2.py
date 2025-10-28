2323eba176a9bf2d4450de0ae21b39d39f226710
artemkoleda1 <Koleda_AV1_25@student.grsu.by>
2025-10-28 09:18:43 +0300

    правый треугольник

--- 4.2.py
+++ 4.2.py
@@ -3,3 +3,8 @@ def draw_rectangle(rows, columns, ch):
         for j in range(columns):
             print(ch, end="")
         print()
+def draw_right_triangle(rows, ch):
+    for i in range(1, rows + 1):
+        for j in range(i):
+            print(ch, end="")
+        print()
